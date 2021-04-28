46c17944e89240a5a606e4c5b09ab70ce4bc85ae
YooseokSo <koreanso@naver.com>
2021-04-28 14:59:13 +0900

    minsdk 21로 변경 파이어베이스 data insert 추가

--- app/src/main/java/com/example/course/MainActivity.kt
+++ app/src/main/java/com/example/course/MainActivity.kt
@@ -2,13 +2,25 @@ package com.example.course
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
+    var fbFirestore : FirebaseFirestore? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fbFirestore = FirebaseFirestore.getInstance()
+
+        if(true)
+        {
+            var stemps = ModelStemps()
+
+            fbFirestore?.collection("stamp")?.document()?.set(stemps)
+        }
     }
 
 
---
+++ app/src/main/java/com/example/course/ModelStemps.kt
@@ -0,0 +1,8 @@
+package com.example.course
+data class ModelStemps (var sid : String? = null,
+                        var uid : String? = null,
+                        var tid : String? = null,
+                        var address : String? = null,
+                        var sName: String? = null,
+                        var sdate : String? = null){
+}
